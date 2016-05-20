653099a95bd18f1132efd5e8aa50f040e0e4daf0
Pierre Chanial <pierre.chanial@gmail.com>
2016-05-20 02:28:45 +0200

    Python 3 fix.

--- pyoperators/utils/misc.py
+++ pyoperators/utils/misc.py
@@ -101,7 +101,10 @@ def deprecated(msg):
             def __init__(self, *args, **keywords):
                 warn('Class {!r} is deprecated: {}'.format(x.__name__, msg),
                      PyOperatorsDeprecationWarning)
-                x.__init__.__func__(self, *args, **keywords)
+                if sys.version_info.major > 2:
+                    x.__init__(self, *args, **keywords)
+                else:
+                    x.__init__.__func__(self, *args, **keywords)
             __init__.__doc__ = x.__init__.__doc__
             return type(x.__name__, x.__bases__, {'__init__': __init__,
                                                   '__doc__': x.__doc__})
